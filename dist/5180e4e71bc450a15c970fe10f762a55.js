9939e6f10f8919d62c49700d78ee8caabcd3b241
RuiHui <wg1055759@swccd.edu>
2018-01-23 12:56:41 +0800

    成功跳转页面

--- dist/5180e4e71bc450a15c970fe10f762a55.js
+++ dist/5180e4e71bc450a15c970fe10f762a55.js
@@ -65,7 +65,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({7:[function(require,module,exports) {
+})({9:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -128,7 +128,7 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":7}],0:[function(require,module,exports) {
+},{"./bundle-url":9}],0:[function(require,module,exports) {
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
 function Module() {
@@ -146,7 +146,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent) {
-  var ws = new WebSocket('ws://localhost:59575/');
+  var ws = new WebSocket('ws://localhost:60333/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
